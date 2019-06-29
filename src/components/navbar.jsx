6efb7576c7178935bdeca7c3542794c3af505102
m.choromzadeh <chorom.mamad@gmail.com>
2019-06-29 19:09:25 +0430

    create Navbar components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
-
-
+import React from 'react';
+import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.css';
+import '../CSS/navbar.css'
 const NavBar = () => {
     return ( 
-        <nav className='na'
+        <nav className="navbar-expand-lg navbar-light bg-light">
+            <a className='navbar-brand ml-5' href="#">
+                <img src='https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg' className="p-1" width='15%' height='15%'></img>
+                pooneh
+            </a>
+            {/* <div className="rtl collapse navbar-collapse" id="navbarSupportedContent"> */}
+                <ul class="rtl navbar-nav mr-auto p-4 mr-5">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">ورود   | <span class="sr-only">(current)</span></a>
+                    </li>
+                    
+                    <li class="nav-item mr-0">
+                        <a class="nav-link" href="#">عضویت</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link ml-4" href="#">راهنما</a>
+                    </li>
+                </ul>
+            {/* </div> */}
+
+        </nav>
      );
 }
  
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.css';
-import "font-awesome/css/font-awesome.css"
+import "font-awesome/css/font-awesome.css";
+import NavBar from './components/navbar';
 
-render(<App />, document.getElementById('root'));
+
+render(<NavBar />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
